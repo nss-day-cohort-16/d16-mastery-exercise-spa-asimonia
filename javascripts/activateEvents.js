c36221dc16cc590ef5ac41a7162c69b42fb7a767
asimonia <alex.simonian@gmail.com>
2016-11-08 00:03:04 -0600

    Completed several issues

--- javascripts/activateEvents.js
+++ javascripts/activateEvents.js
@@ -1,7 +1,33 @@
 var CarLot = (function (oldCarLot) {
 
 	oldCarLot.activateEvents = function () {
-		console.log("Yes");
+		console.log("event activated");
+		var cards = document.getElementsByClassName("inventory-card");
+		var description = document.getElementById("description");
+		var edit;
+
+		for (let i = 0; i < cards.length; i++) {
+			cards[i].addEventListener("click", function(event) {
+				document.getElementById("description").focus();
+				edit = event.currentTarget;
+				for (let z = 0; z < cards.length; z++) {
+					CarLot.nullify(cards[z]);
+				}
+				document.getElementById("description").value = "";
+				CarLot.highlight(cards[i], "red");
+			});
+		}
+
+		description.addEventListener("keyup", function (event) {
+			if (!edit) {
+				alert("Choose a car");
+				description.value = "";
+			} else {
+				description = event.target.value;
+				edit.getElementsByClassName("description")[0].innerHTML = description;
+			}
+		});
+
 	};
 
 	return oldCarLot;
